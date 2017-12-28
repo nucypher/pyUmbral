62527ef2c295cd6aeea52102faa6fd24d3008c41
David Nuñez <davidnunezmontanez@gmail.com>
2017-12-28 02:06:20 +0100

    Basic setup

---
+++ setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup, Extension
+
+INSTALL_REQUIRES = ['msgpack-python', 'cryptography'] #'pysha3', 
+
+TESTS_REQUIRE = [
+    'pytest',
+    'coverage',
+    'pytest-cov',
+    'pdbpp',
+    'ipython'
+]
+
+setup(name='umbral',
+      version='0.1',
+      description='Umbral PRE implementation for NuCypher KMS',
+      #ext_modules=[elliptic_curve],
+      extras_require={'testing': TESTS_REQUIRE},
+      install_requires=INSTALL_REQUIRES,
+      packages=['umbral'])

03d2a678cfdffc03d2d718b60503cef5c2f0ede0
tuxxy <john@nucypher.com>
2018-01-19 03:42:53 -0700

    Implement classmethod load_key for UmbralPrivate/PublicKey classes

--- umbral/keys.py
+++ umbral/keys.py
@@ -1,6 +1,41 @@
+import base64
+
+from cryptography.hazmat.primitives.asymmetric import ec
+from umbral.umbral import UmbralParameters
+from umbral.point import Point, BigNum
+
+
 class UmbralPrivateKey(object):
-    pass
+    def __init__(self, bn_key: BigNum):
+        """
+        Initializes an Umbral private key.
+        """
+        self.bn_key = bn_key
+
+    @classmethod
+    def load_key(cls, key_data: str, params: UmbralParameters):
+        """
+        Loads an Umbral private key from a urlsafe base64 encoded string.
+        """
+        key_bytes = base64.urlsafe_b64decode(key_data)
+
+        bn_key = BigNum.from_bytes(key_bytes, params.curve)
+        return cls(bn_key)
 
 
 class UmbralPublicKey(object):
-    pass
+    def __init__(self, point_key):
+        """
+        Initializes an Umbral public key.
+        """
+        self.point_key = point_key
+
+    @classmethod
+    def load_key(cls, key_data: str, params: UmbralParameters):
+        """
+        Loads an Umbral public key from a urlsafe base64 encoded string.
+        """
+        key_bytes = base64.urlsafe_b64decode(key_data)
+
+        point_key = Point.from_bytes(key_bytes, params.curve)
+        return cls(point_key)

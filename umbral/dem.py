405a93cd357fe83351820b462ea589edb6f9b60c
tuxxy <john@nucypher.com>
2018-01-24 14:52:49 -0700

    Add tests for DEM and some fixes

--- umbral/dem.py
+++ umbral/dem.py
@@ -2,15 +2,19 @@ import os
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 
+DEM_KEYSIZE = 32
+DEM_NONCE_SIZE = 12
+
+
 class UmbralDEM(object):
     def __init__(self, symm_key: bytes):
         """
         Initializes an UmbralDEM object. Requires a key to perform
         ChaCha20-Poly1305.
         """
-        if len(symm_key) != 32:
+        if len(symm_key) != DEM_KEYSIZE:
             raise ValueError(
-                "Invalid key size, must be {} bytes".format(SecretBox.KEY_SIZE)
+                "Invalid key size, must be {} bytes".format(DEM_KEYSIZE)
             )
 
         self.cipher = ChaCha20Poly1305(symm_key)
@@ -19,7 +23,7 @@ class UmbralDEM(object):
         """
         Encrypts data using ChaCha20-Poly1305 with optional authenticated data.
         """
-        nonce = os.urandom(12)
+        nonce = os.urandom(DEM_NONCE_SIZE)
         enc_data = self.cipher.encrypt(nonce, data, authenticated_data)
         return nonce + enc_data
 
@@ -28,7 +32,7 @@ class UmbralDEM(object):
         Decrypts data using ChaCha20-Poly1305 and validates the provided
         authenticated data.
         """
-        nonce = enc_data[:12]
-        ciphertext = enc_data[12:]
+        nonce = enc_data[:DEM_NONCE_SIZE]
+        ciphertext = enc_data[DEM_NONCE_SIZE:]
         plaintext = self.cipher.decrypt(nonce, ciphertext, authenticated_data)
         return plaintext

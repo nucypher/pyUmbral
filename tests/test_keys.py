73666b21540b038903ba98269c9a40728e40f7e0
tuxxy <john@nucypher.com>
2018-02-01 01:47:59 -0700

    Have tests not pass in params by default except for one

--- tests/test_keys.py
+++ tests/test_keys.py
@@ -1,47 +1,46 @@
 import pytest
 
 from umbral import umbral, keys
+from umbral.params import UmbralParameters
 
 
 def test_gen_key():
-    umbral_priv_key = keys.UmbralPrivateKey.gen_key(umbral.UmbralParameters())
+    # Pass in the parameters to test that manual param selection works
+    umbral_priv_key = keys.UmbralPrivateKey.gen_key(UmbralParameters())
     assert type(umbral_priv_key) == keys.UmbralPrivateKey
 
-    umbral_pub_key = umbral_priv_key.get_pub_key(umbral.UmbralParameters())
+    umbral_pub_key = umbral_priv_key.get_pub_key(UmbralParameters())
     assert type(umbral_pub_key) == keys.UmbralPublicKey
 
 
 def test_private_key_serialization():
-    pre = umbral.PRE(umbral.UmbralParameters())
+    pre = umbral.PRE()
 
     priv_key = pre.gen_priv()
     umbral_key = keys.UmbralPrivateKey(priv_key)
 
     encoded_key = umbral_key.save_key()
 
-    decoded_key = keys.UmbralPrivateKey.load_key(encoded_key,
-                                                 umbral.UmbralParameters())
+    decoded_key = keys.UmbralPrivateKey.load_key(encoded_key)
 
     assert priv_key == decoded_key.bn_key
 
 
 def test_private_key_serialization_with_encryption():
-    pre = umbral.PRE(umbral.UmbralParameters())
+    pre = umbral.PRE()
 
     priv_key = pre.gen_priv()
     umbral_key = keys.UmbralPrivateKey(priv_key)
 
     encoded_key = umbral_key.save_key(password=b'test')
 
-    decoded_key = keys.UmbralPrivateKey.load_key(encoded_key,
-                                                 umbral.UmbralParameters(),
-                                                 password=b'test')
+    decoded_key = keys.UmbralPrivateKey.load_key(encoded_key, password=b'test')
 
     assert priv_key == decoded_key.bn_key
 
 
 def test_public_key_serialization():
-    pre = umbral.PRE(umbral.UmbralParameters())
+    pre = umbral.PRE()
 
     priv_key = pre.gen_priv()
     pub_key = pre.priv2pub(priv_key)
@@ -50,7 +49,6 @@ def test_public_key_serialization():
 
     encoded_key = umbral_key.save_key()
 
-    decoded_key = keys.UmbralPublicKey.load_key(encoded_key,
-                                                umbral.UmbralParameters())
+    decoded_key = keys.UmbralPublicKey.load_key(encoded_key)
 
     assert pub_key == decoded_key.point_key

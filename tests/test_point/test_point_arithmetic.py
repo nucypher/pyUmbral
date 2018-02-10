2bd8aa9a303a88da9e04570316cdfd388390ba9a
Kieran R Prasch <kieranprasch@gmail.com>
2018-02-09 20:11:36 -0800

    Adds test coverage for bignum arithmetic with mock openssl backend

--- tests/test_point/test_point_arithmetic.py
+++ tests/test_point/test_point_arithmetic.py
@@ -1,92 +1,24 @@
-import contextlib
-
-import pytest
-from cryptography.hazmat.backends.openssl import backend
-from cryptography.hazmat.primitives.asymmetric import ec
-
 from umbral.bignum import BigNum
 from umbral.config import default_curve
 from umbral.point import Point
 
-curve = default_curve() or ec.SECP256K1()
+CURVE = default_curve()
 
 
-@pytest.fixture()
-def mock_openssl(mocker, random_ec_point1, random_ec_bignum):
-    @contextlib.contextmanager
-    def mocked_openssl_backend():
-        def mocked_ec_point_equality(group, ec_point, other_point, bn_ctx):
-            assert 'BN_CTX' in str(bn_ctx)
-            assert 'EC_GROUP' in str(group)
-            assert 'EC_POINT' in str(ec_point)
-            assert 'EC_POINT' in str(other_point)
-
-            assert random_ec_point1.group == group
-            assert random_ec_point1.ec_point == ec_point
-            assert random_ec_point1.ec_point == other_point
-
-            return 1
-
-        def mocked_ec_point_addition(group, sum, ec_point, other_point, bn_ctx):
-            assert 'BN_CTX' in str(bn_ctx)
-            assert 'EC_POINT' in str(other_point)
-            assert 'EC_POINT' in str(sum)
-
-            assert random_ec_point1.group == group
-            assert random_ec_point1.ec_point == ec_point
-
-            return 1
-
-        def mocked_ec_point_multiplication(group, product, null, ec_point, bignum, bn_ctx):
-            assert 'BN_CTX' in str(bn_ctx)
-            assert 'EC_GROUP' in str(group)
-            assert 'EC_POINT' in str(ec_point)
-            assert 'EC_POINT' in str(product)
-            assert 'NULL' in str(null)
-
-            assert random_ec_point1.group == group
-            assert random_ec_point1.ec_point == ec_point
-
-            assert random_ec_bignum.bignum == bignum
-            assert random_ec_bignum.curve_nid == random_ec_point1.curve_nid
-
-            return 1
-
-        def mocked_ec_point_inversion(group, inverse, bn_ctx):
-            assert 'EC_POINT' in str(inverse)
-            assert 'BN_CTX' in str(bn_ctx)
-
-            assert random_ec_point1.group == group
-
-            return 1
-
-        mock_load = {'EC_POINT_mul': mocked_ec_point_multiplication,
-                     'EC_POINT_cmp': mocked_ec_point_equality,
-                     'EC_POINT_add': mocked_ec_point_addition,
-                     'EC_POINT_invert': mocked_ec_point_inversion}
-
-        with contextlib.ExitStack() as stack:
-            for method, patch in mock_load.items():
-                stack.enter_context(mocker.mock_module.patch.object(backend._lib, method, patch))
-            yield
-
-    return mocked_openssl_backend
-
-
-def test_mocked_openssl_point_arithmetic(mock_openssl, random_ec_point1, random_ec_point2, random_ec_bignum):
+def test_mocked_openssl_point_arithmetic(mock_openssl, random_ec_point1, random_ec_point2, random_ec_bignum1):
     with mock_openssl():
         _ = random_ec_point1 == random_ec_point1   # __eq__
-        _ = random_ec_point1 * random_ec_bignum    # __mul__
+        _ = random_ec_point1 * random_ec_bignum1   # __mul__
         _ = random_ec_point1 + random_ec_point2    # __add__
-        _ = ~random_ec_point1                      # __invert__
+        _ = random_ec_point1                       # __invert__
 
 
 def test_point_curve_mult_regression():
     k256_point_bytes = b'\x03\xe0{\x1bQ\xbf@\x1f\x95\x8d\xe1\x17\xa7\xbe\x9e-G`T\xbf\xd7\x9e\xa7\x10\xc8uA\xc0z$\xc0\x92\x8a'
     k256_bn_bytes = b'4u\xd70-\xa0h\xdeG\xf0\x143\x06!\x91\x05{\xe4jC\n\xf1h\xed7a\xf8\x9d\xec^\x19\x8c'
 
-    k256_point = Point.from_bytes(k256_point_bytes, curve=curve)
-    k256_bn = BigNum.from_bytes(k256_bn_bytes, curve=curve)
+    k256_point = Point.from_bytes(k256_point_bytes, curve=CURVE)
+    k256_bn = BigNum.from_bytes(k256_bn_bytes, curve=CURVE)
 
     product_with_star_operator = k256_point * k256_bn
 

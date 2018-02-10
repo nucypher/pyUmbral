d61d266342a9635bdf746218c9f58c3b02b1c99b
Kieran R Prasch <kieranprasch@gmail.com>
2018-02-10 14:19:47 -0800

    Additional point and bignum assertions for type checking. Marks xfail for multicurve simple api.

--- tests/test_bignum.py
+++ tests/test_bignum.py
@@ -2,17 +2,24 @@ from umbral.bignum import BigNum
 
 
 def test_mocked_openssl_bignum_arithmetic(mock_openssl, random_ec_bignum1, random_ec_bignum2):
+
+    operations_that_construct = (
+        random_ec_bignum1 * random_ec_bignum2,           # __mul__
+        random_ec_bignum1 ** random_ec_bignum2,          # __pow__
+        random_ec_bignum1 ** int(random_ec_bignum2),     # __pow__ (as int)
+        random_ec_bignum1 + random_ec_bignum2,           # __add__
+        random_ec_bignum1 - random_ec_bignum2,           # __sub__
+        random_ec_bignum1 % random_ec_bignum2,           # __mod__
+        random_ec_bignum1 % int(random_ec_bignum2) ,     # __mod__ (as int)
+        ~random_ec_bignum1,                              # __invert__
+        # assert random_ec_bignum1 / random_ec_bignum2   # __truediv__
+    )
+
     with mock_openssl():
-        _ = random_ec_bignum1 == random_ec_bignum1          # __eq__
-        _ = random_ec_bignum1 * random_ec_bignum2           # __mul__
-        _ = random_ec_bignum1 ** random_ec_bignum2          # __pow__
-        _ = random_ec_bignum1 ** int(random_ec_bignum2)     # __pow__ (as int)
-        _ = random_ec_bignum1 + random_ec_bignum2           # __add__
-        _ = random_ec_bignum1 - random_ec_bignum2           # __sub__
-        _ = random_ec_bignum1 % random_ec_bignum2           # __mod__
-        _ = random_ec_bignum1 % int(random_ec_bignum2)      # __mod__ (as int)
-        _ = ~random_ec_bignum1                              # __invert__
-        # _ = random_ec_bignum1 / random_ec_bignum2           # __truediv__
+        assert random_ec_bignum1 == random_ec_bignum1    # __eq__
+        for operator_result in operations_that_construct:
+            assert operator_result
+            assert isinstance(operator_result, BigNum)
 
 
 def test_cast_bignum_to_int():

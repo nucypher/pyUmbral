e4ffeac66c9b694963e0182429dba8232ba70e56
Kieran R Prasch <kieranprasch@gmail.com>
2018-02-09 20:11:36 -0800

    Adds test coverage for bignum arithmetic with mock openssl backend

--- tests/test_bignum.py
+++ tests/test_bignum.py
@@ -1,24 +1,33 @@
 from umbral.bignum import BigNum
-from cryptography.hazmat.primitives.asymmetric import ec
 from umbral.config import default_curve
 
+CURVE = default_curve()
 
 
-def test_from_to_int():
-    curve = default_curve() or ec.SECP256K1()
-    x = BigNum.gen_rand(curve)
+def test_mocked_openssl_bignum_arithmetic(mock_openssl, random_ec_bignum1, random_ec_bignum2):
+    with mock_openssl():
+        _ = random_ec_bignum1 == random_ec_bignum1     # __eq__
+        _ = random_ec_bignum1 * random_ec_bignum2      # __mul__
+        _ = random_ec_bignum1 ** random_ec_bignum2     # __pow__
+        _ = random_ec_bignum1 + random_ec_bignum2      # __add__
+        _ = random_ec_bignum1 - random_ec_bignum2      # __sub__
+        _ = random_ec_bignum1 % random_ec_bignum2      # __mod__
+        _ = ~random_ec_bignum1                         # __invert__
 
-    xint = x.__int__()
-    
-    y = BigNum.from_int(xint, curve)
 
+def test_cast_bignum_to_int():
+    x = BigNum.gen_rand(CURVE)
+
+    x_as_int_from_dunder = x.__int__()
+    x_as_int_type_caster = int(x)
+    assert x_as_int_from_dunder == x_as_int_type_caster
+    x = x_as_int_type_caster
+
+    y = BigNum.from_int(x, CURVE)
     assert x == y
 
 
 def test_bn_to_cryptography_privkey():
-    curve = ec.SECP256K1()
-    bn = BigNum.gen_rand(curve)
-
+    bn = BigNum.gen_rand(CURVE)
     crypto_privkey = bn.to_cryptography_priv_key()
-
     assert int(bn) == crypto_privkey.private_numbers().private_value 

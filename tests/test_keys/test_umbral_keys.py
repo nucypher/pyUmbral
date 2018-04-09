8bdd40b0991ce42d5482938140dd7dcfe403f21d
David Nuñez <davidnunezmontanez@gmail.com>
2018-04-09 12:13:15 +0200

    Add tests for derived keys generation

--- tests/test_keys/test_umbral_keys.py
+++ tests/test_keys/test_umbral_keys.py
@@ -9,6 +9,32 @@ def test_gen_key():
     umbral_pub_key = umbral_priv_key.get_pubkey()
     assert type(umbral_pub_key) == keys.UmbralPublicKey
 
+def test_derive_key_from_label():
+    master_secret = b"random master secret"
+    label = b"my_healthcare_information"
+
+    priv_key1 = keys.UmbralPrivateKey.derive_key_from_label(master_secret, label)
+    assert type(priv_key1) == keys.UmbralPrivateKey
+
+    pub_key1 = priv_key1.get_pubkey()
+    assert type(pub_key1) == keys.UmbralPublicKey
+
+
+    priv_key2 = keys.UmbralPrivateKey.derive_key_from_label(master_secret, label)
+    pub_key2 = priv_key2.get_pubkey()
+    assert priv_key1.bn_key == priv_key2.bn_key
+    assert pub_key1 == pub_key2
+
+    # A salt can be used too, but of course it affects the derived key
+    salt = b"optional, randomly generated salt"
+    priv_key3 = keys.UmbralPrivateKey.derive_key_from_label(master_secret, label, salt=salt)
+    assert priv_key3.bn_key != priv_key1.bn_key
+
+    # Different labels on the same master secret create different keys
+    label = b"my_tax_information"
+    priv_key4 = keys.UmbralPrivateKey.derive_key_from_label(master_secret, label)
+    pub_key4 = priv_key4.get_pubkey()
+    assert priv_key1.bn_key != priv_key4.bn_key
 
 def test_private_key_serialization(random_ec_bignum1):
     priv_key = random_ec_bignum1

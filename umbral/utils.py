5b41cd65bfa91bb4901169c72ac3d9e9b0e85c29
tuxxy <john@nucypher.com>
2018-04-24 16:52:09 -0600

    Use better method for determining keysize in bytes (thanks @cygnusv)

--- umbral/utils.py
+++ umbral/utils.py
@@ -41,6 +41,4 @@ def kdf(ecpoint, key_length):
 
 
 def get_curve_keysize_bytes(curve):
-    # We use the ceil operation to fit all bytes on curve sizes where eight is
-    # not evenly divisible.
-    return int(math.ceil(curve.key_size / 8.00))
+    return (curve.key_size + 7) // 8

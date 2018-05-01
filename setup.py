ebe0c8fa042670946357a752c1694586cc41d0a0
jMyles <justin@justinholmes.com>
2018-05-01 14:29:55 -0700

    Removing cryptography.io from setup.py; using python markers in Pipfile to use wheels.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup, Extension
 
-INSTALL_REQUIRES = ['msgpack-python', 'cryptography', 'pynacl'] #'pysha3', 
+INSTALL_REQUIRES = ['msgpack-python', 'pynacl'] #'pysha3',# TODO: Add cryptography wheel
 
 TESTS_REQUIRE = [
     'pytest',

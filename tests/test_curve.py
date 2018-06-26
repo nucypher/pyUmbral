f80f128927084eea1e0fb026df427f7da1947684
tuxxy <john@nucypher.com>
2018-06-26 14:01:01 -0600

    Create supported_curves property and check values in test

--- tests/test_curve.py
+++ tests/test_curve.py
@@ -5,10 +5,10 @@ from umbral.curve import Curve, SECP256R1, SECP256K1, SECP384R1
 
 def test_curve_whitelist():
     # Test the AVAIL_CURVES dict to have only these three curves:
-    assert len(Curve.AVAIL_CURVES) == 3
-    assert Curve.AVAIL_CURVES['secp256r1'] == 415
-    assert Curve.AVAIL_CURVES['secp256k1'] == 714
-    assert Curve.AVAIL_CURVES['secp384r1'] == 715
+    assert len(Curve._Curve__AVAIL_CURVES) == 3
+    assert Curve._Curve__AVAIL_CURVES['secp256r1'] == 415
+    assert Curve._Curve__AVAIL_CURVES['secp256k1'] == 714
+    assert Curve._Curve__AVAIL_CURVES['secp384r1'] == 715
 
     # Test that we can't instantiate other curves:
     with pytest.raises(ValueError):
@@ -18,6 +18,11 @@ def test_curve_whitelist():
     test_p256 = SECP256R1()
     test_secp256k1 = SECP256K1()
     test_p384 = SECP384R1()
+
+    # Test the supported curves property
+    assert test_p256.supported_curves\
+    == test_secp256k1.supported_curves\
+    == test_p384.supported_curves
 
     assert test_p256.curve_nid == 415
     assert test_secp256k1.curve_nid == 714
--- umbral/curve.py
+++ umbral/curve.py
@@ -8,14 +8,19 @@ class Curve:
     as a convenient whitelist to limit the curves used in pyUmbral.
     """
 
-    AVAIL_CURVES = {
+    __AVAIL_CURVES = {
         'secp256r1': 415,
         'secp256k1': 714,
         'secp384r1': 715,
     }
 
     def __init__(self, curve_nid: int):
-        if curve_nid not in self.AVAIL_CURVES.values():
+        """
+        Instantiates an OpenSSL curve with the provided curve_nid and derives
+        the proper EC_GROUP struct and order. You can _only_ instantiate curves
+        with supported nids (see `Curve.supported_curves`).
+        """
+        if curve_nid not in self.__AVAIL_CURVES.values():
             raise ValueError(
                 "Curve NID passed ({}) is not supported.".format(curve_nid))
 
@@ -24,13 +29,17 @@ class Curve:
         self.ec_group = openssl._get_ec_group_by_curve_nid(self.curve_nid)
         self.order = openssl._get_ec_order_by_curve_nid(self.curve_nid)
 
+    @property
+    def supported_curves(self):
+        return self.__AVAIL_CURVES
+
 
 class SECP256R1(Curve):
     """
     Instantiates a NIST secp256r1 (P-256) curve.
     """
     def __init__(self):
-        super().__init__(super().AVAIL_CURVES['secp256r1'])
+        super().__init__(self.supported_curves['secp256r1'])
 
 
 class SECP256K1(Curve):
@@ -39,7 +48,7 @@ class SECP256K1(Curve):
     This is the default curve currently used in NuCypher.
     """
     def __init__(self):
-        super().__init__(super().AVAIL_CURVES['secp256k1'])
+        super().__init__(self.supported_curves['secp256k1'])
 
 
 class SECP384R1(Curve):
@@ -47,4 +56,4 @@ class SECP384R1(Curve):
     Instantiates a NIST secp384r1 (P-384) curve.
     """
     def __init__(self):
-        super().__init__(super().AVAIL_CURVES['secp384r1'])
+        super().__init__(self.supported_curves['secp384r1'])

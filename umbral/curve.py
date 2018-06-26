e8c5c1b4491b416d18ec00aacb73f790a7cbe065
tuxxy <john@nucypher.com>
2018-06-26 02:43:44 -0600

    Raise ValueError if Curve is instantiated with unsupported nid

--- umbral/curve.py
+++ umbral/curve.py
@@ -1,20 +1,24 @@
 from umbral import openssl
 
 
-CURVE_WHITELIST = {
-    'secp256r1': 415,
-    'secp256k1': 714,
-    'secp384r1': 715,
-}
-
-
 class Curve:
     """
     Acts as a container to store constant variables such as the OpenSSL
     curve_nid, the EC_GROUP struct, and the order of the curve. This also acts
-    as a convenient interface to limit the curves used in pyUmbral.
+    as a convenient whitelist to limit the curves used in pyUmbral.
     """
+
+    AVAIL_CURVES = {
+        'secp256r1': 415,
+        'secp256k1': 714,
+        'secp384r1': 715,
+    }
+
     def __init__(self, curve_nid: int):
+        if curve_nid not in self.AVAIL_CURVES.values():
+            raise ValueError(
+                "Curve NID passed ({}) is not supported.".format(curve_nid))
+
         # TODO: Limit the creations of EC_GROUP structs in openssl.py
         self.curve_nid = curve_nid
         self.ec_group = openssl._get_ec_group_by_curve_nid(self.curve_nid)
@@ -26,15 +30,16 @@ class SECP256R1(Curve):
     Instantiates a NIST secp256r1 (P-256) curve.
     """
     def __init__(self):
-        super().__init__(CURVE_WHITELIST['secp256r1'])
+        super().__init__(super().AVAIL_CURVES['secp256r1'])
 
 
 class SECP256K1(Curve):
     """
     Instantiates a SECG secp256k1 curve.
+    This is the default curve currently used in NuCypher.
     """
     def __init__(self):
-        super().__init__(CURVE_WHITELIST['secp256k1'])
+        super().__init__(super().AVAIL_CURVES['secp256k1'])
 
 
 class SECP384R1(Curve):
@@ -42,4 +47,4 @@ class SECP384R1(Curve):
     Instantiates a NIST secp384r1 (P-384) curve.
     """
     def __init__(self):
-        super().__init__(CURVE_WHITELIST['secp384r1'])
+        super().__init__(super().AVAIL_CURVES['secp384r1'])

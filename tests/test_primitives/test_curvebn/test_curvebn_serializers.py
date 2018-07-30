1941d38c687b4c4a851248daab4a097240d3974a
David Núñez <david@nucypher.com>
2018-07-30 12:20:40 +0200

    Test for CurveBN serialization now tests deserialization and multicurve

--- tests/test_primitives/test_curvebn/test_curvebn_serializers.py
+++ tests/test_primitives/test_curvebn/test_curvebn_serializers.py
@@ -1,16 +1,19 @@
 import pytest
 
 from umbral.curvebn import CurveBN
+from umbral.curve import CURVES
 
-def test_serialize_rotations_of_1():
-    
-    for i in range(32):
+@pytest.mark.parametrize("curve", CURVES)
+def test_serialization_rotations_of_1(curve):
 
+    size_in_bytes = CurveBN.expected_bytes_length(curve)
+    for i in range(size_in_bytes):
         lonely_one = 1 << i
-        bn = CurveBN.from_int(lonely_one)
-        assert bn.to_bytes() == lonely_one.to_bytes(32, 'big')
+        bn = CurveBN.from_int(lonely_one, curve)
+        lonely_one_in_bytes = lonely_one.to_bytes(size_in_bytes, 'big')
 
+        # Check serialization
+        assert bn.to_bytes() == lonely_one_in_bytes
 
-        
-
-
+        # Check deserialization
+        assert CurveBN.from_bytes(lonely_one_in_bytes, curve) == bn

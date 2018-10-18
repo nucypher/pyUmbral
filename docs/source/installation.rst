e8c8ae52d4515121a36ce04eada5e9f26a63dd13
David Núñez <david@nucypher.com>
2018-10-19 01:05:11 +0200

    Bump version: 0.1.1-alpha.1 → 0.1.1-alpha.2

--- docs/source/installation.rst
+++ docs/source/installation.rst
@@ -1,7 +1,7 @@
 
 Installing pyUmbral
 ====================
-v0.1.1-alpha.1
+v0.1.1-alpha.2
 
 
 Acquire the source code
--- umbral/__about__.py
+++ umbral/__about__.py
@@ -10,7 +10,7 @@ __url__ = "https://github.com/nucypher/pyUmbral"
 
 __summary__ = 'NuCypher\'s Umbral Proxy Re-Encryption Implementation',
 
-__version__ = "0.1.1-alpha.1"
+__version__ = "0.1.1-alpha.2"
 
 __author__ = "NuCypher"
 
